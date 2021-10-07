2af006e954996e116df1a14310eaae9b402517e6
Juan Esteban Nichoy L <juan.nichoy@mawesi.net>
2021-10-06 22:16:59 -0500

    Repositories created

---
+++ Aplicacion/FalconInventorySystem.App/FalconInventorySystem.App.Persistence/Interfaces/IRepositorySupplier.cs
@@ -0,0 +1,17 @@
+﻿using FalconInventorySystem.App.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FalconInventorySystem.App.Persistence.Interfaces
+{
+    public interface IRepositorySupplier
+    {
+        Task<IEnumerable<Supplier>> GetSuppliersAll();
+        Task<Supplier> AddSuppliers(Supplier supplier);
+        Task<Supplier> GetSupplierById(int id);
+        Task<Boolean> UpdateSupplier(Supplier supplier);
+        Task<Boolean> DeleteSupplier(int id);
+    }
+}
