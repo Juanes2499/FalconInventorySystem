7f41b2776887e4ac8ed38254fa8562e44d541082
DanielRl <verde3991@gmail.com>
2021-10-08 18:22:22 -0500

    daniel error

--- Aplicacion/FalconInventorySystem.App/FalconInventorySystem.App.Frontend/wwwroot/js/site.js
+++ Aplicacion/FalconInventorySystem.App/FalconInventorySystem.App.Frontend/wwwroot/js/site.js
@@ -2,3 +2,22 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+/* funcion para el popup de los borones de crear  */
+var btnAbrirPopup = document.getElementById('btn-abrir-popup'),
+	overlay = document.getElementById('overlay'),
+	popup = document.getElementById('popup'),
+	btnCerrarPopup = document.getElementById('btn-cerrar-popup');
+
+btnAbrirPopup.addEventListener('click', function(){
+	overlay.classList.add('active');
+	popup.classList.add('active');
+});
+
+btnCerrarPopup.addEventListener('click', function(e){
+	e.preventDefault();
+	overlay.classList.remove('active');
+	popup.classList.remove('active');
+});
+
+/* --------------------------------------*/
