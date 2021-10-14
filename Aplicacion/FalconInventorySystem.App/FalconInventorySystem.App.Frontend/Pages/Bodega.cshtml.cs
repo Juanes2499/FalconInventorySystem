36df905b975f748a262c912db970824f000505c4
BrayanJMB <milian.brayanc@gmail.com>
2021-10-13 21:58:02 -0500

    front-end

--- Aplicacion/FalconInventorySystem.App/FalconInventorySystem.App.Frontend/Pages/Bodega.cshtml.cs
+++ Aplicacion/FalconInventorySystem.App/FalconInventorySystem.App.Frontend/Pages/Bodega.cshtml.cs
@@ -4,22 +4,44 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
+using FalconInventorySystem.App.Domain.Entities;
+using FalconInventorySystem.App.Persistence.Interfaces;
 
 namespace FalconInventorySystem.App.Frontend.Pages
 {
     public class BodegaModel : PageModel
     {
-        private readonly ILogger<BodegaModel> _logger;
+       private readonly IRepositoryWarehouse repositoryWarehouse;
 
-        public BodegaModel(ILogger<BodegaModel> logger)
+        [BindProperty]
+        public Warehouse WareHouse { get; set; }
+        public List<Warehouse> WareHouseList { get; set; }
+
+        public BodegaModel(IRepositoryWarehouse repositoryWarehouse)
         {
-            _logger = logger;
+            this.repositoryWarehouse = repositoryWarehouse;
+        }
+
+        public IEnumerable<Warehouse> GetWareHouses()
+        {
+            var warehouseList = repositoryWarehouse.GetAllWarehouses();
+            return warehouseList;
         }
 
         public void OnGet()
         {
+            WareHouseList = new List<Warehouse>();
+            WareHouseList.AddRange(GetWareHouses());
+        }
 
+        public IActionResult OnPost()
+        {
+            var newWareHouse = WareHouse;
+            var WareHouseCreated = repositoryWarehouse.CreateWarehouse(newWareHouse);
+            WareHouse = null;
+            WareHouseList = new List<Warehouse>();
+            WareHouseList.AddRange(GetWareHouses());
+            return RedirectToPage("Bodega");
         }
     }
 }
